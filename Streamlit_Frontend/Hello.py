11546b65a7c5e994343906f8b639ecb200a243a4
Rafiya Rehan <rafiya.21rehan@gamil.com>
2024-12-06 03:10:51 +0500

    UI change

--- Streamlit_Frontend/Hello.py
+++ Streamlit_Frontend/Hello.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hi",
+    page_title="Meal_Plan",
     page_icon="👋",
 )
 
 st.write("# Welcome to Diet Plan Recommendation System! 👋")
 
-st.sidebar.success("Select a recommendation app.")
+#st.sidebar.success("Select a recommendation app.")
 
 st.markdown(
     """
     Diet Plan Recommendation System
-    Compose your Meal plans!
+
+    Compose your Meal plan!
     """
 )

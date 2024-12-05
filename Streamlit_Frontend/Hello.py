91b0dd7904b1098e73713e1a073d51437ad30cb2
Rafiya Rehan <rafiya.21rehan@gamil.com>
2024-12-06 02:52:57 +0500

    Initial commit

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hi",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Diet Plan Recommendation System! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    Diet Plan Recommendation System
+    Compose your Meal plans!
+    """
+)
